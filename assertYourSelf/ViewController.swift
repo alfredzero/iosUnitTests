ea6b7e170a10529b07108c5b54b404c0b104317d
alfredzero <amezcuaalfredo81@gmail.com>
2024-05-30 23:20:54 -0700

    ios unit test with message

---
+++ assertYourSelf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assertYourSelf
+//
+//  Created by Alfredo Amezcua on 5/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ assertYourSelfTests/assertYourSelfTests.swift
@@ -0,0 +1,15 @@
+//
+//  assertYourSelfTests.swift
+//  assertYourSelfTests
+//
+//  Created by Alfredo Amezcua on 5/30/24.
+//
+
+import XCTest
+@testable import assertYourSelf
+
+final class assertYourSelfTests: XCTestCase {
+    func test_fail() {
+   XCTFail("This is a setup to fail")
+    }
+}
